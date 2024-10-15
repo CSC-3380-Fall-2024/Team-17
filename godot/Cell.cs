591ce5ffa704c6a3b362074063c9360058ece6a6
Starlumine <jchan1star@gmail.com>
2024-10-15 02:00:08 -0500

    Translate Cell.gd to C#
    Converted the Cell class from GDScript to C#

--- godot/Cell.cs
+++ godot/Cell.cs
@@ -3,19 +3,42 @@ using System;
 
 public class Cell : Area
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
+	private MeshInstance topFace;
+	private MeshInstance northFace;
+	private MeshInstance eastFace;
+	private MeshInstance southFace;
+	private MeshInstance westFace;
+	private MeshInstance bottomFace;
+	
 	public override void _Ready()
 	{
-		
+		topFace = GetNode<MeshInstance>("TopFace");
+		northFace = GetNode<MeshInstance>("NorthFace");
+		eastFace = GetNode<MeshInstance>("EastFace");
+		southFace = GetNode<MeshInstance>("SouthFace");
+		westFace = GetNode<MeshInstance>("WestFace");
+		bottomFace = GetNode<MeshInstance>("BottomFace");
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public void UpdateFaces(Godot.Collections.Dictionary<Vector2, Cell> cellList)
+	{
+		Vector2 myGridPosition = new Vector2(Translation.x / Globals.GRID_SIZE, Translation.z / 2);
+
+		if (cellList.ContainsKey(myGridPosition + Vector2.Right))
+		{
+			eastFace.QueueFree();
+		}
+		if (cellList.ContainsKey(myGridPosition + Vector2.Left))
+		{
+			westFace.QueueFree();
+		}
+		if (cellList.ContainsKey(myGridPosition + Vector2.Down))
+		{
+			southFace.QueueFree();
+		}
+		if (cellList.ContainsKey(myGridPosition + Vector2.Up))
+		{
+			northFace.QueueFree();
+		}
+	}
 }
