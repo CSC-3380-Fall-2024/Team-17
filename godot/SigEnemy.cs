0cdcd706873e60af46bf5fd87e826be53ec838e0
ghebert3 <54825353+ghebert3@users.noreply.github.com>
2024-11-22 20:33:07 -0600

    Made Signals for Enemies and Player
    Connected Player and Enemy Signals

--- godot/SigEnemy.cs
+++ godot/SigEnemy.cs
@@ -3,4 +3,22 @@ using System;
 
 public class SigEnemy : Node2D
 
-
+{
+	public void OnAttack()
+	{
+		GD.Print("Enemy Attack");
+	}
+	
+	public void OnDmgTaken()
+	{
+		GD.Print("Enemy Taken Damage");
+	}
+	
+	public void OnDmgDealt()
+	{
+		GD.Print("Enemy Hits Player");
+	}
+	
+	
+	
+}
