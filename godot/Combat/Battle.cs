dc445f1834143cbc67a2351bd208c6fda817dc35
Ioan Yordanov <iyordanov888@gmail.com>
2024-11-23 00:37:09 -0600

    Update to Battle.cs ig 😧

--- godot/Combat/Battle.cs
+++ godot/Combat/Battle.cs
@@ -3,186 +3,229 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-
+using static SigEnemy;
+using static SigPlayer;
+using static SigContrScript;
+using System.Runtime.Serialization;
 public class Battle : Control
 {
-	public class Skill
-	{
-		private int damage;
-		private int hitrate;
-		public bool mag;
-		public Skill(int damage, int hitrate, bool mag)
-		{
-			damage = this.damage;
-			hitrate = this.hitrate;
-			mag = this.mag;
-		}
+    [Signal]
+	public delegate void Attack();
+    private SigPlayer sigPlayer;
+    public class Skill
+    {
+        private int damage;
+        private int hitrate;
+        public bool mag;
+        public Skill(int damage, int hitrate, bool mag)
+        {
+            damage = this.damage;
+            hitrate = this.hitrate;
+            mag = this.mag;
+        }
 
-		public int GetDamage()
-		{
-			return damage;
-		}
-	}
-	public class SkillList
-	{
-		public Skill ice1 = new Skill(30, 90, true);
-		public Skill fire1 = new Skill(30, 90, true);
-		public Skill elec1 = new Skill(30,90, true);
-		public Skill stab1 = new Skill(40, 80, false);
-		protected List<Skill> allSkills;
+        public int GetDamage()
+        {
+            return damage;
+        }
+    }
+    public class SkillList
+    {
+        public Skill ice1 = new Skill(30, 90, true);
+        public Skill fire1 = new Skill(30, 90, true);
+        public Skill elec1 = new Skill(30,90, true);
+        public Skill stab1 = new Skill(40, 80, false);
+        public List<Skill> allSkills = new List<Skill>();
 
-		public SkillList()
-		{
-			allSkills.Add(ice1);
-			allSkills.Add(fire1);
-			allSkills.Add(elec1);
-			allSkills.Add(stab1);
-		}
+        public SkillList()
+        {   
+            allSkills.Add(ice1);
+            allSkills.Add(fire1);
+            allSkills.Add(elec1);
+            allSkills.Add(stab1);
+        }
 
 
-	}
-	public class CombatChar
-	{
-		public SkillList totalSkills = new SkillList();
-		protected int attack;
-		protected int def;
-		protected int mag;
-		protected int speed;
-		protected int luck;
-		protected int health;
-		protected List<Skill> skillList;
+    }
+    public class CombatChar
+    {
+        public SkillList totalSkills = new SkillList();
+        protected int attack;
+        protected int def;
+        protected int mag;
+        protected int speed;
+        protected int luck;
+        public int health;
+        protected List<Skill> skillList;
 
-		public CombatChar(){}//empty constructor so that way subclasses can be passed
+        public CombatChar(){}//empty constructor so that way subclasses can be passed
 
-		public CombatChar(int attack, int def, int mag, int speed, int luck, int health)
-		{
-			attack = this.attack;
-			def = this.def;
-			mag = this.mag;
-			speed = this.speed;
-			luck = this.luck;
-			health = this.health;
+        public CombatChar(int attack, int def, int mag, int speed, int luck, int health)
+        {
+            attack = this.attack;
+            def = this.def;
+            mag = this.mag;
+            speed = this.speed;
+            luck = this.luck;
+            health = this.health;
 
-		}
+        }
 
-		public int BaseAttack()
-		{
-			return attack/2;
-		}
+        public int BaseAttack()
+        {
+            return attack/2;
+        }
 
-		public int MagAttack(Skill magSkill)
-		{
-			return (mag*magSkill.GetDamage())/2;
-		}
+        public int SkillAttack(Skill skill)
+        {
+            if (skill.mag)
+            {
+                return (mag*skill.GetDamage())/2;
+            }
+            else
+            {
+                return (attack*skill.GetDamage())/2;
+            }
+        }
 
-		public int PhysAttack(Skill physSkill)
-		{
-			return (attack*physSkill.GetDamage())/2;
-		}
+        public int DamageCalc(int damage)
+        {
+            return damage/def;
+        }
 
-		public int DamageCalc(int damage)
-		{
-			return damage/def;
-		}
+        public void AddSkillList(ref List<Skill> sList, Skill skill)
+        {
+            if (sList.Contains(skill))
+            {
+                GD.PrintErr("Error, skill already in skill list");
+                return;
+            }
+            else
+            {
+                sList.Add(skill);
+                GD.Print($"Skill {skill} has been added");
+            }
+        }
+        public void Damage(int damageCalc, CombatChar combatChar)
+        {
+            combatChar.health -= damageCalc;
+            
+        }
+        public int GetSpeed()
+        {
+            return speed;
+        }
 
-		public void AddSkillList(ref List<Skill> sList, Skill skill)
-		{
-			if (sList.Contains(skill))
-			{
-				GD.PrintErr("Error, skill already in skill list");
-				return;
-			}
-			else
-			{
-				sList.Add(skill);
-				GD.Print($"Skill {skill} has been added");
-			}
-		}
-		public int Damage(int damageCalc)
-		{
-			health -= damageCalc;
-			return damageCalc;
-		}
-		public int GetSpeed()
-		{
-			return speed;
-		}
+    }
+    public class CombatPlayer:CombatChar
+    {
+        
+        public CombatPlayer(int attack, int def, int mag, int speed, int luck, int health)
+        {
+            attack = this.attack;
+            def = this.def;
+            mag = this.mag;
+            speed = this.speed;
+            luck = this.luck;
+            health = this.health;
+        }
+    }
 
-	}
-	public class CombatPlayer:CombatChar
-	{
-		
-		public CombatPlayer(int attack, int def, int mag, int speed, int luck, int health)
-		{
-			attack = this.attack;
-			def = this.def;
-			mag = this.mag;
-			speed = this.speed;
-			luck = this.luck;
-			health = this.health;
-		}
-	}
+    public class CombatEnemy:CombatChar
+    {
+        public CombatEnemy(int attack, int def, int mag, int speed, int luck, int health)
+        {
+            attack = this.attack;
+            def = this.def;
+            mag = this.mag;
+            speed = this.speed;
+            luck = this.luck;
+            health = this.health;
+        }
 
-	public class CombatEnemy:CombatChar
-	{
-		public CombatEnemy(int attack, int def, int mag, int speed, int luck, int health)
-		{
-			attack = this.attack;
-			def = this.def;
-			mag = this.mag;
-			speed = this.speed;
-			luck = this.luck;
-			health = this.health;
-		}
+        public int DoNothing(){return 0;}//enemy does nothing
 
-		public void DoNothing(){}//enemy does nothing
+        public int EnemyChoiceSel()
+        {
+            Random rand = new Random();
+            int randNum = rand.Next(11);
+            if (randNum >= 8)
+            {
+                if (skillList.Count() > 0)
+                {
+                    randNum = rand.Next(skillList.Count()+1);
+                    return SkillAttack(skillList[randNum]);
+                }
+                else
+                {
+                    return BaseAttack();
+                }
+            }
+            else if (randNum >=3)
+            {
+                return BaseAttack();
+            }
+            else
+            {
+                return DoNothing();
+            }
+        }
+    }
 
-		public void EnemyChoiceSel()
-		{
-			Random rand = new Random();
-			int randNum = rand.Next(11);
-			if (randNum >= 8)
-			{
-				if (skillList.Count() > 0)
-				{
-					randNum = rand.Next(skillList.Count()+1);
-					if (skillList.ElementAt<Skill>(randNum).mag)
-					{
-						MagAttack(skillList.ElementAt<Skill>(randNum));
-					}
-					else
-					{
-						PhysAttack(skillList.ElementAt<Skill>(randNum));
-					}
-					return;
-				}
-			}
-			else if (randNum >=3)
-			{
-				BaseAttack();
-				return;
-			}
-			else
-			{
-				DoNothing();
-			}
-		}
-	}
+    public static void TurnOrder(ref List<CombatChar> allChars)
+    {
+        allChars.Sort((x,y) => x.GetSpeed().CompareTo(y.GetSpeed()));
 
-	public static void TurnOrder(ref List<CombatChar> allChars)
-	{
-		allChars.Sort((x,y) => x.GetSpeed().CompareTo(y.GetSpeed()));
+    }
+    public override void _Ready()
+    {
+        SigPlayer sPlayer = GetNode<SigPlayer>("res://SigPlayer");
+        CombatPlayer player = new CombatPlayer(3, 5, 3, 5, 1, 100);
+        CombatChar ally1 = new CombatChar(3, 3, 3, 3, 4, 100);
+        CombatChar ally2 = new CombatChar(3, 3, 3, 3, 3, 100);
+        CombatChar ally3 = new CombatChar(3, 3, 3, 3, 2, 100);
+        CombatEnemy enemy = new CombatEnemy(3, 2, 3, 1, 1, 100);
+        List<CombatChar> turnOrder = new List<CombatChar> { player, ally1, ally2, ally3, enemy };
+        TurnOrder(ref turnOrder);
+        GD.Print($"the turn order is {turnOrder[0]},{turnOrder[1]},{turnOrder[2]},{turnOrder[3]},{turnOrder[4]}");
+        Button aButton = GetNode<Button>("Attack");
+        Button skillButton = GetNode<Button>("Skill");
+        while(enemy.health != 0 || player.health != 0)
+        {
+            Queue<Action> turnQueue = new Queue<Action>();
+            while(turnQueue.Count() > 5)
+            {
+                int i = 0;
+                CombatChar x = turnOrder[i];
+                if (x is CombatEnemy)
+                {
+                    CombatEnemy y = x as CombatEnemy;
+                    Action enemyAttck = delegate() {y.Damage(y.DamageCalc(y.EnemyChoiceSel()), player);};
+                    turnQueue.Enqueue(enemyAttck);
+                    i++;
 
-	}
-	
-	public override void _Ready()
-	{
-		//get enemy from tscn
-		//get allies from tscn
-		List<CombatChar> allChars; //append all characters here
-		//List<CombatChar> turnOrder = allChars;
-		//TurnOrder(turnOrder);
+                }
+                else
+                {
+                    if (aButton.Pressed)
+                    {
+                        GD.Print("button pressed");
+                        Action attck = delegate() {x.Damage(x.DamageCalc(x.BaseAttack()), enemy);};
+                        turnQueue.Enqueue(attck);
+                        i++;
+                    }
+                }
 
-	}
+            }
+            GD.Print("Combat started");
+            foreach (Action i in turnQueue)
+            {
+                i();
+            }
+            GD.Print("New Turn!");
+        }
+
+    }
 
 }
+
