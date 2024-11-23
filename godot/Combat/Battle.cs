556f860cd0b31a3c1e631eaf401d224e18e1376d
Starlumine <jchan1star@gmail.com>
2024-11-22 21:10:29 -0600

    Added a signal to open up the attack menu
    Can now click the "enemy appears" message so that the actions menu appears.

--- godot/Combat/Battle.cs
+++ godot/Combat/Battle.cs
@@ -1,22 +1,45 @@
 using Godot;
 using System;
 
+
 public class Battle : Control
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
+	// Creates a signal when the textbox is closed
+	[Signal]
+	public delegate void TextboxClosed();
+	
 	public override void _Ready()
 	{
 		GetNode<Control>("Textbox").Hide();
+		GetNode<Control>("ActionsPanel").Hide();	
+		
+		display_text("A wild enemy appears!");
+		
+		Connect(nameof(TextboxClosed), this, nameof(OnTextboxClosed));
 		
 	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	
+	public override void _Input(InputEvent @event)
+	{
+		// Checks that the left mouse button is pressed
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == (int)ButtonList.Left && mouseEvent.Pressed)
+		{
+			GetNode<Control>("Textbox").Hide();
+			EmitSignal(nameof(TextboxClosed));
+		}
+	}
+	
+	private void display_text(string text)
+	{
+		GetNode<Control>("Textbox").Show();
+		GetNode<Label>("Textbox/Label").Text = text;
+	}
+	
+	private void OnTextboxClosed()
+	{
+		// Show the ActionsPanel when the textbox is closed
+		GetNode<Control>("ActionsPanel").Show();
+		GD.Print("Textbox closed!"); 
+	}
 }
+
