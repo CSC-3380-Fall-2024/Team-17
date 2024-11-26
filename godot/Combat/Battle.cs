0bfd69187a9d64715fa0bf75f7fcd89f5ffabf62
Ioan Yordanov <iyordanov888@gmail.com>
2024-11-26 14:08:04 -0600

    Got Combat Working to a Degree
    Only got the player's basic attacks working but the menu changes and it at least can be presentable

--- godot/Combat/Battle.cs
+++ godot/Combat/Battle.cs
@@ -13,9 +13,48 @@ public class Battle : Control
 	[Signal]
 	public delegate void TextboxClosed();
 	[Signal]
-	public delegate void Attack();
-	private SigPlayer sigPlayer;
-	
+	public delegate void Atk(); //does an attack
+	[Signal]
+	public delegate void SkillAtk(); //does a skill attack
+	[Signal]
+	public delegate void OptionSelected();//player has chosen their option
+	// [Signal]
+	// public delegate void Defend();
+	// [Signal]
+	// public delegate void Item();
+	// [Signal]
+	// public delegate void Switch();
+	// [Signal]
+	// public delegate void Run();
+	[Signal]
+	public delegate void DmgTaken();
+	[Signal]
+	public delegate void DmgDealt();
+	[Signal]
+	public delegate void FinishedSel();//this will kick off the combat
+	[Signal]
+	public delegate void Missed();
+	[Signal]
+	public delegate void UpdateSkills();//takes the name of a combatChar as an arg and the updates the skil list for that char
+	[Signal]
+	public delegate void NextCharGoes();
+	[Signal]
+	public delegate void EnemyChoiceSel();
+	[Signal]
+	public delegate void HealthUpdate1();
+	[Signal]
+	public delegate void HealthUpdate2();
+	[Signal]
+	public delegate void HealthUpdate3();
+	[Signal]
+	public delegate void HealthUpdate4();
+	[Signal]
+	public delegate void EHealthUpdate();
+	public Queue<string> turnQueue = new Queue<string>();
+	public List<CombatChar> turnOrder = new List<CombatChar>();
+	public List<CombatChar> partyMem = new List<CombatChar>();
+	public bool startSig;
+	private CombatChar enemy;
 	public override void _Input(InputEvent @event)
 	{
 		// Checks that the left mouse button is pressed
@@ -38,229 +77,144 @@ public class Battle : Control
 		GetNode<Control>("ActionsPanel").Show();
 		
 	}
-
-	
-	public class Skill
-	{
-		private int damage;
-		private int hitrate;
-		public bool mag;
-		public Skill(int damage, int hitrate, bool mag)
-		{
-			damage = this.damage;
-			hitrate = this.hitrate;
-			mag = this.mag;
-		}
-
-		public int GetDamage()
-		{
-			return damage;
-		}
-	}
-	public class SkillList
-	{
-		public Skill ice1 = new Skill(30, 90, true);
-		public Skill fire1 = new Skill(30, 90, true);
-		public Skill elec1 = new Skill(30,90, true);
-		public Skill stab1 = new Skill(40, 80, false);
-		public List<Skill> allSkills = new List<Skill>();
-
-		public SkillList()
-		{   
-			allSkills.Add(ice1);
-			allSkills.Add(fire1);
-			allSkills.Add(elec1);
-			allSkills.Add(stab1);
-		}
-
-
-	}
-	public class CombatChar
-	{
-		public SkillList totalSkills = new SkillList();
-		protected int attack;
-		protected int def;
-		protected int mag;
-		protected int speed;
-		protected int luck;
-		public int health;
-		protected List<Skill> skillList;
-
-		public CombatChar(){}//empty constructor so that way subclasses can be passed
-
-		public CombatChar(int attack, int def, int mag, int speed, int luck, int health)
-		{
-			attack = this.attack;
-			def = this.def;
-			mag = this.mag;
-			speed = this.speed;
-			luck = this.luck;
-			health = this.health;
-
-		}
-
-		public int BaseAttack()
-		{
-			return attack/2;
-		}
-
-		public int SkillAttack(Skill skill)
-		{
-			if (skill.mag)
-			{
-				return (mag*skill.GetDamage())/2;
-			}
-			else
-			{
-				return (attack*skill.GetDamage())/2;
-			}
-		}
-
-		public int DamageCalc(int damage)
-		{
-			return damage/def;
-		}
-
-		public void AddSkillList(ref List<Skill> sList, Skill skill)
-		{
-			if (sList.Contains(skill))
-			{
-				GD.PrintErr("Error, skill already in skill list");
-				return;
-			}
-			else
-			{
-				sList.Add(skill);
-				GD.Print($"Skill {skill} has been added");
-			}
-		}
-		public void Damage(int damageCalc, CombatChar combatChar)
-		{
-			combatChar.health -= damageCalc;
-			
-		}
-		public int GetSpeed()
-		{
-			return speed;
-		}
-
-	}
-	public class CombatPlayer:CombatChar
-	{
-		
-		public CombatPlayer(int attack, int def, int mag, int speed, int luck, int health)
-		{
-			attack = this.attack;
-			def = this.def;
-			mag = this.mag;
-			speed = this.speed;
-			luck = this.luck;
-			health = this.health;
-		}
-	}
-
-	public class CombatEnemy:CombatChar
-	{
-		public CombatEnemy(int attack, int def, int mag, int speed, int luck, int health)
-		{
-			attack = this.attack;
-			def = this.def;
-			mag = this.mag;
-			speed = this.speed;
-			luck = this.luck;
-			health = this.health;
-		}
-
-		public int DoNothing(){return 0;}//enemy does nothing
-
-		public int EnemyChoiceSel()
-		{
-			Random rand = new Random();
-			int randNum = rand.Next(11);
-			if (randNum >= 8)
-			{
-				if (skillList.Count() > 0)
-				{
-					randNum = rand.Next(skillList.Count()+1);
-					return SkillAttack(skillList[randNum]);
-				}
-				else
-				{
-					return BaseAttack();
-				}
-			}
-			else if (randNum >=3)
-			{
-				return BaseAttack();
-			}
-			else
-			{
-				return DoNothing();
-			}
-		}
-	}
-
 	public static void TurnOrder(ref List<CombatChar> allChars)
 	{
-		allChars.Sort((x,y) => x.GetSpeed().CompareTo(y.GetSpeed()));
+		allChars.Sort((x,y) => y.GetSpeed().CompareTo(x.GetSpeed()));
 
 	}
+	
 	public override void _Ready()
 	{
 		GetNode<Control>("Textbox").Hide();
-		GetNode<Control>("ActionsPanel").Hide();	
-		
+		GetNode<Control>("ActionsPanel").Hide();
+		Connect(nameof(OptionSelected), this, nameof(OnSkillAtk));
 		display_text("A wild enemy appears!");
-		
+		Connect(nameof(FinishedSel),this, nameof(ReadySig));
 		Connect(nameof(TextboxClosed), this, nameof(OnTextboxClosed));
-
-		SigPlayer sPlayer = GetNode<SigPlayer>("res://SigPlayer");
-		CombatPlayer player = new CombatPlayer(3, 5, 3, 5, 1, 100);
-		CombatChar ally1 = new CombatChar(3, 3, 3, 3, 4, 100);
-		CombatChar ally2 = new CombatChar(3, 3, 3, 3, 3, 100);
-		CombatChar ally3 = new CombatChar(3, 3, 3, 3, 2, 100);
-		CombatEnemy enemy = new CombatEnemy(3, 2, 3, 1, 1, 100);
-		List<CombatChar> turnOrder = new List<CombatChar> { player, ally1, ally2, ally3, enemy };
-		TurnOrder(ref turnOrder);
-		GD.Print($"the turn order is {turnOrder[0]},{turnOrder[1]},{turnOrder[2]},{turnOrder[3]},{turnOrder[4]}");
-		Button aButton = GetNode<Button>("Attack");
-		Button skillButton = GetNode<Button>("Skill");
-		while(enemy.health != 0 || player.health != 0)
-		{
-			Queue<Action> turnQueue = new Queue<Action>();
-			while(turnQueue.Count() > 5)
-			{
-				int i = 0;
-				CombatChar x = turnOrder[i];
-				if (x is CombatEnemy)
-				{
-					CombatEnemy y = x as CombatEnemy;
-					Action enemyAttck = delegate() {y.Damage(y.DamageCalc(y.EnemyChoiceSel()), player);};
-					turnQueue.Enqueue(enemyAttck);
-					i++;
-
-				}
-				else
-				{
-					if (aButton.Pressed)
-					{
-						GD.Print("button pressed");
-						Action attck = delegate() {x.Damage(x.DamageCalc(x.BaseAttack()), enemy);};
-						turnQueue.Enqueue(attck);
-						i++;
-					}
-				}
-
-			}
-			GD.Print("Combat started");
-			foreach (Action i in turnQueue)
-			{
-				i();
-			}
-			GD.Print("New Turn!");
-		}
+		SkillList sList = new SkillList();
+		CombatPlayer player = new CombatPlayer("PLAYER","debug");
+		CombatChar ally1 = new CombatChar("ALLY1", "debug");
+		CombatChar ally2 = new CombatChar("ALLY2", "debug");
+		CombatChar ally3 = new CombatChar("ALLY3", "debug");
+		CombatChar ally4 = new CombatChar("ALLY4", "debug");
+		enemy = new CombatEnemy("ENEMY", "debug");	
+		turnOrder = new List<CombatChar> { player, ally1, ally2, ally3, enemy};
+		//TurnOrder(ref turnOrder);
+		GD.Print($"the turn order is {turnOrder[0].name},{turnOrder[1].name},{turnOrder[2].name},{turnOrder[3].name},{turnOrder[4].name}");
+		partyMem = new List<CombatChar> {player, ally1, ally2, ally3};
+		startSig = false;
+		//EmitSignal(nameof(Atk),player.DamageCalc(player.BaseAttack(),enemy),enemy);
+		GD.Print("Combat Start!");
+		EmitSignal(nameof(UpdateSkills),"PLAYER");
 
 	}
+	 public override void _Process(float delta)
+ 	{
+		SkillList sList = new SkillList();
+		List<string> strList = sList.GetAllSkills();
+		if(turnQueue.Count.Equals(4) && startSig)
+			{
+				foreach(string i in turnQueue)
+				{
+					GD.Print(i);
+				}
+				EmitSignal(nameof(EnemyChoiceSel));
+				while(turnQueue.Count != 0 && startSig)
+				{
+					int j = 0;
+					string i = turnQueue.Peek();
+					Random rand = new Random();
+					int x = rand.Next(4);
+					switch (i)
+					{
+						case "attack":
+						EmitSignal(nameof(Atk),turnOrder[j].DamageCalc(turnOrder[j].BaseAttack(),enemy),enemy);
+						display_text(turnOrder[j].name + "is attacking enemy for " + turnOrder[j].BaseAttack());
+						break;
+						case "eAttack":
+						EmitSignal(nameof(Atk),enemy.DamageCalc(enemy.BaseAttack(),partyMem[x]),partyMem[x]);
+						display_text(enemy.name + "is attacking " + partyMem[x].name + " for " + enemy.BaseAttack());
+						break;
+						case "ice1":
+						EmitSignal(nameof(SkillAtk),turnOrder[j].DamageCalc(turnOrder[j].SkillAttack(sList.GetSkill((string)i)),enemy));
+						display_text(turnOrder[j].name + "activates spell " + i +" for "+ turnOrder[j].SkillAttack(sList.GetSkill(i)));
+						break;
+						case "fire1":
+						EmitSignal(nameof(SkillAtk),turnOrder[j].DamageCalc(turnOrder[j].SkillAttack(sList.GetSkill((string)i)),enemy));
+						display_text(turnOrder[j].name + "activates spell " + i +" for "+ turnOrder[j].SkillAttack(sList.GetSkill(i)));
+						break;
+						case "elec1":
+						EmitSignal(nameof(SkillAtk),turnOrder[j].DamageCalc(turnOrder[j].SkillAttack(sList.GetSkill((string)i)),enemy));
+						display_text(turnOrder[j].name + "activates spell " + i +" for "+ turnOrder[j].SkillAttack(sList.GetSkill(i)));
+						break;
+						case "stab1":
+						EmitSignal(nameof(SkillAtk),turnOrder[j].DamageCalc(turnOrder[j].SkillAttack(sList.GetSkill((string)i)),enemy));
+						display_text(turnOrder[j].name + "activates spell " + i +" for "+ turnOrder[j].SkillAttack(sList.GetSkill(i)));
+						break;
+						case "eice1":
+						EmitSignal(nameof(SkillAtk),turnOrder[j].DamageCalc(enemy.SkillAttack(sList.GetSkill((string)i)),partyMem[x]));
+						display_text(enemy.name + "activates spell " + i +" at "+ turnOrder[x].name + " for " +enemy.SkillAttack(sList.GetSkill(i)));
+						break;
+						case "efire1":
+						EmitSignal(nameof(SkillAtk),turnOrder[j].DamageCalc(turnOrder[j].SkillAttack(sList.GetSkill((string)i)),enemy));
+						display_text(enemy.name + "activates spell " + i +" at "+ turnOrder[x].name + " for " +enemy.SkillAttack(sList.GetSkill(i)));
+						break;
+						case "eelec1":
+						EmitSignal(nameof(SkillAtk),turnOrder[j].DamageCalc(turnOrder[j].SkillAttack(sList.GetSkill((string)i)),enemy));
+						display_text(enemy.name + "activates spell " + i +" at "+ turnOrder[x].name + " for " +enemy.SkillAttack(sList.GetSkill(i)));
+						break;
+						case "eStab1":
+						EmitSignal(nameof(SkillAtk),turnOrder[j].DamageCalc(turnOrder[j].SkillAttack(sList.GetSkill((string)i)),enemy));
+						display_text(enemy.name + "activates spell " + i +" at "+ turnOrder[x].name + " for " +enemy.SkillAttack(sList.GetSkill(i)));
+						break;
+					}
+					j++;
+					turnQueue.Dequeue();
+				}
+				startSig = false;
+				GetNode<Control>("Textbox").Hide();
+				EmitSignal(nameof(NextCharGoes),false);
+				EmitSignal(nameof(UpdateSkills),"PLAYER");
+				EmitSignal(nameof(HealthUpdate1), turnOrder[0].health);
+				EmitSignal(nameof(HealthUpdate2), turnOrder[1].health);
+				EmitSignal(nameof(HealthUpdate3), turnOrder[2].health);
+				EmitSignal(nameof(HealthUpdate4), turnOrder[3].health);
+				EmitSignal(nameof(EHealthUpdate), enemy.health);
+				GD.Print("New Turn!");
+			}
+			if(turnQueue.Count == 0)
+			{
+				if((partyMem[0] != null && partyMem[0].health < 0) || (enemy != null && enemy.health < 0))
+				{
+					GD.Print("Combat Ended");
+					GetTree().ChangeScene("res://World.tscn");
+				}
+			}
+		}
+		
+		
 
+	private void _on_Attack_pressed()
+	{
+		turnQueue.Enqueue("attack");
+		GD.Print("Attacked");
+		EmitSignal(nameof(NextCharGoes),true);
+	}
+	private void _on_Skill_pressed()
+	{
+		GetNode<PopupMenu>("SkillMenuPopup").Popup_();
+	}	
+	private void OnSkillAtk(string skill)
+	{
+		turnQueue.Enqueue(skill);
+	}
+	private void SigEnemyChoiceSel(string choice)
+	{
+		turnQueue.Enqueue(choice);
+	}
+	private void ReadySig()
+	{
+		display_text("Combat Is About to Start");
+		startSig = true;
+	}
 }
-
