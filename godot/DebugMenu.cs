5dc5a854d9fed8dbe08985a68682201613b55689
Starlumine <jchan1star@gmail.com>
2024-12-07 15:57:39 -0600

    Added three buttons into the debug scene
    Added buttons for changing the aspect ratio.

--- godot/DebugMenu.cs
+++ godot/DebugMenu.cs
@@ -31,12 +31,31 @@ public class DebugMenu : Control
 		GetNode<Control>("Floor Generator").Show();
 		GetNode<Control>("Room Select").Show();
 	}
+	private void _on_Aspect_Ratio_pressed()
+	{
+		GetNode<Control>("RoomSelector").Hide();
+		GetNode<Control>("Title Subtext").Hide();
+		GetNode<Control>("Floor Generator").Hide();
+		GetNode<Control>("Room Select").Hide();
+		GetNode<Control>("Aspect Ratio2").Show();
+		GetNode<Control>("Aspect Ratio3").Show();
+	}
+	
+	private void _on_Aspect_Ratio2_pressed()
+	{
+		GetNode<Control>("RoomSelector").Hide();
+		GetNode<Control>("Title Subtext").Hide();
+		GetNode<Control>("Floor Generator").Hide();
+		GetNode<Control>("Room Select").Hide();
+	}
 	public override void _Ready()
 	{
 		GetNode<Control>("RoomSelector").Hide();
 		GetNode<Control>("Title Subtext").Show();
 		GetNode<Control>("Floor Generator").Show();
 		GetNode<Control>("Room Select").Show();
+		GetNode<Control>("Aspect Ratio2").Hide();
+		GetNode<Control>("Aspect Ratio3").Hide();
 
 	}
 
